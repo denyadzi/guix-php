1f8747f19f8e69aadea4065f9b01acbaf81f755a
Dzianis <mail@miesta.by>
2021-11-15 10:26:05 +0300

    added mbstring extension

--- guix/php/packages/php-ext.scm
+++ guix/php/packages/php-ext.scm
@@ -3,23 +3,17 @@
   #:use-module (guix packages)
   #:use-module (guix utils)
   #:use-module (gnu packages autotools)
+  #:use-module (gnu packages pkg-config)
   #:use-module (gnu packages pcre)
+  #:use-module (gnu packages textutils)
   #:use-module (guix build-system gnu)
   #:use-module (guix build gnu-build-system)
   #:use-module (php packages php))
 
 (define-public php-ext-fileinfo
   (package
+   (inherit php)
    (name "php-ext-fileinfo")
-   (version (package-version php))
-   (home-page "https://secure.php.net/")
-   (source (origin
-              (method url-fetch)
-              (uri (string-append home-page "/distributions/php-" version ".tar.xz"))
-              (sha256
-               (base32
-                "02pakyc6msnp6d00qcc2i8ppi6rnhy7dj9ga4cr05lqg7dxh20d5"))))
-   (build-system gnu-build-system)
    (arguments
     `(#:configure-flags
       (list (string-append "--with-php-config="
@@ -39,7 +33,7 @@
        (replace 'install 
                 (lambda* (#:key outputs #:allow-other-keys)
                   (let ((lib (string-append (assoc-ref outputs "out") "/lib")))
-                    (install-file "modules/fileinfo.so" (string-append lib "/php-" ,(version-major+minor version) "/extensions"))
+                    (install-file "modules/fileinfo.so" (string-append lib "/php-" ,(version-major+minor (package-version php)) "/extensions"))
                     #t))))))
    (inputs
     `(("php" ,php)
@@ -49,3 +43,41 @@
    (synopsis "PHP fileinfo shared extension")
    (description "PHP fileinfo shared extension")
    (license (package-license php))))
+
+(define-public php-ext-mbstring
+  (package
+   (inherit php)
+   (name "php-ext-mbstring")
+   (arguments
+    `(#:configure-flags
+      (list (string-append "--with-php-config="
+                           (assoc-ref %build-inputs "php") "/bin/php-config"))
+      #:tests? #f
+      #:phases
+      (modify-phases
+       %standard-phases
+       (add-after 'unpack 'chdir-ext
+                  (lambda _
+                    (chdir "ext/mbstring")
+                    #t))
+       (add-before 'configure 'phpize
+                   (lambda* (#:key inputs #:allow-other-keys)
+                     (system (string-append "PHP_AUTOCONF=" (assoc-ref inputs "autoconf") "/bin/autoconf " (assoc-ref inputs "php") "/bin/phpize"))
+                     #t))
+       (replace 'install 
+                (lambda* (#:key outputs #:allow-other-keys)
+                  (let ((lib (string-append (assoc-ref outputs "out") "/lib")))
+                    (install-file "modules/mbstring.so" (string-append lib "/php-" ,(version-major+minor (package-version php)) "/extensions"))
+                    #t))))))
+   (inputs
+    `(("php" ,php)
+      ("oniguruma" ,oniguruma)
+      ("pcre" ,pcre2))) 
+   (native-inputs
+    `(("autoconf" ,autoconf)
+      ("pkg-config" ,pkg-config)))
+   (synopsis "PHP mbstring shared extension")
+   (description "PHP mbstring shared extension")
+   (license (package-license php))))
+
+
